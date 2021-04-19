85971cbdf1b711cdae80a5846cab214c21a7353a
Charles Villard <charles.villard@epita.fr>
2021-04-19 09:52:45 +0200

    esp32: uart: rewrite uart threaded like chibios WIP

--- libraries/AP_HAL_ESP32/UARTDriver.cpp
+++ libraries/AP_HAL_ESP32/UARTDriver.cpp
@@ -12,36 +12,332 @@
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <AP_HAL/AP_HAL.h>
 
-#include <AP_HAL_ESP32/UARTDriver.h>
+#if CONFIG_HAL_BOARD == HAL_BOARD_ESP32 && !defined(HAL_NO_UARTDRIVER)
+#include "UARTDriver.h"
+#include "GPIO.h"
 #include <AP_Math/AP_Math.h>
+#include <AP_InternalError/AP_InternalError.h>
+#include <AP_Common/ExpandingString.h>
+#include "Scheduler.h"
 
 #include "esp_log.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
 
 extern const AP_HAL::HAL& hal;
 
-namespace ESP32 {
+using namespace ESP32;
 
 UARTDesc uart_desc[] = {HAL_ESP32_UART_DEVICES};
 
-void UARTDriver::vprintf(const char *fmt, va_list ap)
-{
+const UARTDriver::SerialDef UARTDriver::_serial_tab[] = { HAL_UART_DEVICE_LIST };
 
+// handle for UART handling thread
+void* volatile UARTDriver::uart_rx_thread_ctx;
+
+// table to find UARTDrivers from serial number, used for event handling
+UARTDriver *UARTDriver::uart_drivers[UART_MAX_DRIVERS];
+
+uint32_t UARTDriver::_last_stats_ms;
+
+#ifndef HAL_UART_MIN_TX_SIZE
+#define HAL_UART_MIN_TX_SIZE 512
+#endif
+
+#ifndef HAL_UART_MIN_RX_SIZE
+#define HAL_UART_MIN_RX_SIZE 512
+#endif
+
+#ifndef HAL_UART_STACK_SIZE
+#define HAL_UART_STACK_SIZE 320
+#endif
+
+#ifndef HAL_UART_RX_STACK_SIZE
+#define HAL_UART_RX_STACK_SIZE 768
+#endif
+
+UARTDriver::UARTDriver(uint8_t _serial_num) :
+serial_num(_serial_num),
+sdef(_serial_tab[_serial_num]),
+_baudrate(57600)
+{
+    configASSERT(serial_num < UART_MAX_DRIVERS, "too many UART drivers");
+    uart_drivers[serial_num] = this;
+}
+
+/*
+  thread for handling UART send/receive
+
+  We use events indexed by serial_num to trigger a more rapid send for
+  unbuffered_write uarts, and run at 1kHz for general UART handling
+ */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic error "-Wframe-larger-than=128"
+void UARTDriver::uart_thread()
+{
+    uint32_t last_thread_run_us = 0; // last time we did a 1kHz run of this uart
+
+    while (uart_thread_ctx == nullptr) {
+        hal.scheduler->delay_microseconds(1000);
+    }
+
+    while (true) {
+        eventmask_t mask = chEvtWaitAnyTimeout(EVT_TRANSMIT_DATA_READY | EVT_TRANSMIT_END | EVT_TRANSMIT_UNBUFFERED, chTimeMS2I(1));
+        uint32_t now = AP_HAL::micros();
+        bool need_tick = false;
+        if (now - last_thread_run_us >= 1000) {
+            // run the timer tick if it's been more than 1ms since we last run
+            need_tick = true;
+            last_thread_run_us = now;
+        }
+
+        // change the thread priority if requested - if unbuffered it should only have higher priority than the owner so that
+        // handoff occurs immediately
+        if (mask & EVT_TRANSMIT_UNBUFFERED) {
+            chThdSetPriority(unbuffered_writes ? MIN(_uart_owner_thd->realprio + 1, APM_UART_UNBUFFERED_PRIORITY) : APM_UART_PRIORITY);
+        }
+        // send more data
+        if (_tx_initialised && ((mask & EVT_TRANSMIT_DATA_READY) || need_tick || (hd_tx_active && (mask & EVT_TRANSMIT_END)))) {
+            _tx_timer_tick();
+        }
+    }
+}
+#pragma GCC diagnostic pop
+
+/*
+  thread for handling UART receive
+
+  We use events indexed by serial_num to trigger a more rapid send for
+  unbuffered_write uarts, and run at 1kHz for general UART handling
+ */
+void UARTDriver::uart_rx_thread(void* arg)
+{
+    while (uart_rx_thread_ctx == nullptr) {
+        hal.scheduler->delay_microseconds(1000);
+    }
+
+    while (true) {
+        hal.scheduler->delay_microseconds(1000);
+
+        for (uint8_t i=0; i<UART_MAX_DRIVERS; i++) {
+            if (uart_drivers[i] == nullptr) {
+                continue;
+            }
+            if (uart_drivers[i]->_rx_initialised) {
+                uart_drivers[i]->_rx_timer_tick();
+            }
+        }
+    }
+}
+
+/*
+  initialise UART RX thread
+ */
+void UARTDriver::thread_rx_init(void)
+{
+    if (uart_rx_thread_ctx == nullptr) {
+        xTaskCreate(uart_rx_thread,
+                "UART_RX",
+                HAL_UART_RX_STACK_SIZE,
+                nullptr,
+                APM_UART_PRIORITY,
+                &uart_rx_thread_ctx);
+
+        if (uart_rx_thread_ctx == nullptr) {
+            AP_HAL::panic("Could not create UART RX thread\n");
+        }
+    }
+}
+
+/*
+  initialise UART TX_thread
+ */
+void UARTDriver::thread_init(void)
+{
+    if (uart_thread_ctx == nullptr) {
+        hal.util->snprintf(uart_thread_name, sizeof(uart_thread_name), sdef.is_usb ? "OTG%1u" : "UART%1u", sdef.instance);
+        xTaskCreate(uart_thread_trampoline,
+                uart_thread_name,
+                HAL_UART_STACK_SIZE,
+                this,
+                unbuffered_writes ? APM_UART_UNBUFFERED_PRIORITY : APM_UART_PRIORITY,
+                &uart_thread_ctx);
+        if (uart_thread_ctx == nullptr) {
+            AP_HAL::panic("Could not create UART TX thread\n");
+        }
+    }
+}
+
+void UARTDriver::uart_thread_trampoline(void* p)
+{
+    UARTDriver* uart = static_cast<UARTDriver*>(p);
+    uart->uart_thread();
+}
+
+#ifndef HAL_STDOUT_SERIAL
+/*
+  hook to allow printf() to work on hal.console when we don't have a
+  dedicated debug console
+ */
+static int hal_console_vprintf(const char *fmt, va_list arg)
+{
+/*
 	uart_port_t p = uart_desc[uart_num].port;
 	if (p == 0)
 		esp_log_writev(ESP_LOG_INFO, "", fmt, ap);
 	else
 		AP_HAL::UARTDriver::vprintf(fmt, ap);
+*/
+    hal.console->vprintf(fmt, arg);
+    return 1; // wrong length, but doesn't matter for what this is used for
 }
-
-void UARTDriver::begin(uint32_t b)
-{
-    begin(b, 0, 0);
-}
-
+#endif
 
 void UARTDriver::begin(uint32_t b, uint16_t rxS, uint16_t txS)
 {
+    thread_rx_init();
+
+    if (sdef.serial == nullptr) {
+        return;
+    }
+    uint16_t min_tx_buffer = HAL_UART_MIN_TX_SIZE;
+    uint16_t min_rx_buffer = HAL_UART_MIN_RX_SIZE;
+
+    /*
+      increase min RX size to ensure we can receive a fully utilised
+      UART if we are running our receive loop at 40Hz. This means 25ms
+      of data. Assumes 10 bits per byte, which is normal for most
+      protocols
+     */
+    bool rx_size_by_baudrate = true;
+    if (rx_size_by_baudrate) {
+        min_rx_buffer = MAX(min_rx_buffer, b/(40*10));
+    }
+
+    if (sdef.is_usb) {
+        // give more buffer space for log download on USB
+        min_tx_buffer *= 2;
+    }
+
+#if HAL_MEM_CLASS >= HAL_MEM_CLASS_500
+    // on boards with plenty of memory we can use larger buffers
+    min_tx_buffer *= 2;
+    min_rx_buffer *= 2;
+#endif
+
+    // on PX4 we have enough memory to have a larger transmit and
+    // receive buffer for all ports. This means we don't get delays
+    // while waiting to write GPS config packets
+    if (txS < min_tx_buffer) {
+        txS = min_tx_buffer;
+    }
+    if (rxS < min_rx_buffer) {
+        rxS = min_rx_buffer;
+    }
+
+    /*
+      allocate the read buffer
+      we allocate buffers before we successfully open the device as we
+      want to allocate in the early stages of boot, and cause minimum
+      thrashing of the heap once we are up. The ttyACM0 driver may not
+      connect for some time after boot
+     */
+    while (_in_rx_timer) {
+        hal.scheduler->delay(1);
+    }
+    if (rxS != _readbuf.get_size()) {
+        _rx_initialised = false;
+        _readbuf.set_size(rxS);
+    }
+
+    bool clear_buffers = false;
+    if (b != 0) {
+        // clear buffers on baudrate change, but not on the console (which is usually USB)
+        if (_baudrate != b && hal.console != this) {
+            clear_buffers = true;
+        }
+        _baudrate = b;
+    }
+
+    if (clear_buffers) {
+        _readbuf.clear();
+    }
+
+    /*
+      allocate the write buffer
+     */
+    while (_in_tx_timer) {
+        hal.scheduler->delay(1);
+    }
+    if (txS != _writebuf.get_size()) {
+        _tx_initialised = false;
+        _writebuf.set_size(txS);
+    }
+
+    if (clear_buffers) {
+        _writebuf.clear();
+    }
+
+    if (sdef.is_usb) {
+#ifdef HAVE_USB_SERIAL
+        /*
+         * Initializes a serial-over-USB CDC driver.
+         */
+        if (!_device_initialised) {
+            if ((SerialUSBDriver*)sdef.serial == &SDU1
+#if HAL_HAVE_DUAL_USB_CDC
+                || (SerialUSBDriver*)sdef.serial == &SDU2
+#endif
+            ) {
+                usb_initialise();
+            }
+            _device_initialised = true;
+        }
+#endif
+    } else {
+#if HAL_USE_SERIAL == TRUE
+        if (_baudrate != 0) {
+            sercfg.speed = _baudrate;
+
+            // start with options from set_options()
+            sercfg.cr1 = _cr1_options;
+            sercfg.cr2 = _cr2_options;
+            sercfg.cr3 = _cr3_options;
+
+            if (!(sercfg.cr2 & USART_CR2_STOP2_BITS)) {
+                sercfg.cr2 |= USART_CR2_STOP1_BITS;
+            }
+            sercfg.ctx = (void*)this;
+
+            sdStart((SerialDriver*)sdef.serial, &sercfg);
+
+        }
+#endif // HAL_USE_SERIAL
+    }
+
+    if (_writebuf.get_size()) {
+        _tx_initialised = true;
+    }
+    if (_readbuf.get_size()) {
+        _rx_initialised = true;
+    }
+    _uart_owner_thd = chThdGetSelfX();
+    // initialize the TX thread if necessary
+    thread_init();
+
+    // setup flow control
+    set_flow_control(_flow_control);
+
+    if (serial_num == 0 && _tx_initialised) {
+#ifndef HAL_STDOUT_SERIAL
+        // setup hal.console to take printf() output
+        vprintf_console_hook = hal_console_vprintf;
+#endif
+    }
+
+//SETUP ESP32
     if (uart_num < ARRAY_SIZE(uart_desc)) {
         uart_port_t p = uart_desc[uart_num].port;
         if (!_initialized) {
@@ -195,5 +491,4 @@ bool UARTDriver::discard_input()
     uart_port_t p = uart_desc[uart_num].port;
 	return uart_flush_input(p) == ESP_OK;
 }
-
-}
+#endif
--- libraries/AP_HAL_ESP32/UARTDriver.h
+++ libraries/AP_HAL_ESP32/UARTDriver.h
@@ -15,13 +15,16 @@
 
 #pragma once
 
-#include <AP_HAL/UARTDriver.h>
 #include <AP_HAL/utility/RingBuffer.h>
-#include <AP_HAL_ESP32/AP_HAL_ESP32.h>
-#include <AP_HAL_ESP32/Semaphores.h>
 
-#include "driver/gpio.h"
-#include "driver/uart.h"
+#include "AP_HAL_ESP32.h"
+#include "Semaphores.h"
+
+#define RX_BOUNCE_BUFSIZE 64U
+#define TX_BOUNCE_BUFSIZE 64U
+
+// enough for uartA to uartI, plus IOMCU
+#define UART_MAX_DRIVERS 10
 
 namespace ESP32 {
 
@@ -34,16 +37,12 @@ struct UARTDesc {
 class UARTDriver : public AP_HAL::UARTDriver {
 public:
 
-	UARTDriver(uint8_t serial_num)
-		: AP_HAL::UARTDriver()
-	{
-		_initialized = false;
-		uart_num = serial_num;
-	}
+	UARTDriver(uint8_t serial_num);
 
-	virtual ~UARTDriver() = default;
+    /* Do not allow copies */
+    UARTDriver(const UARTDriver &other) = delete;
+    UARTDriver &operator=(const UARTDriver&) = delete;
 
-	void vprintf(const char *fmt, va_list ap) override;
 
     void begin(uint32_t b) override;
     void begin(uint32_t b, uint16_t rxS, uint16_t txS) override;
@@ -53,37 +52,186 @@ public:
     void set_blocking_writes(bool blocking) override;
     bool tx_pending() override;
 
+
     uint32_t available() override;
-    //uint32_t available_locked(uint32_t key) override;
+    uint32_t available_locked(uint32_t key) override;
 
     uint32_t txspace() override;
-
     int16_t read() override;
-    //ssize_t read(uint8_t *buffer, uint16_t count) override;
-    //int16_t read_locked(uint32_t key) override;
+    ssize_t read(uint8_t *buffer, uint16_t count) override;
+    int16_t read_locked(uint32_t key) override;
+    void _rx_timer_tick();
+    void _tx_timer_tick();
 
-    void _timer_tick(void) override;
+    bool discard_input() override; // discard all bytes available for reading
 
     size_t write(uint8_t c) override;
     size_t write(const uint8_t *buffer, size_t size) override;
 
-    bool discard_input() override; // discard all bytes available for reading
 
-    //bool lock_port(uint32_t write_key, uint32_t read_key) override;
+    // lock a port for exclusive use. Use a key of 0 to unlock
+    bool lock_port(uint32_t write_key, uint32_t read_key) override;
 
-    //size_t write_locked(const uint8_t *buffer, size_t size, uint32_t key) override;
+    // control optional features
+    bool set_options(uint16_t options) override;
+    uint8_t get_options(void) const override;
+
+
+    // write to a locked port. If port is locked and key is not correct then 0 is returned
+    // and write is discarded
+    size_t write_locked(const uint8_t *buffer, size_t size, uint32_t key) override;
+
+    struct SerialDef {
+        uart_port_t serial;
+        uint8_t instance;
+        bool is_usb;
+
+        gpio_num_t tx_line;
+        gpio_num_t rx_line;
+        gpio_num_t rts_line;
+        gpio_num_t cts_line;
+        int8_t rxinv_gpio;
+        uint8_t rxinv_polarity;
+        int8_t txinv_gpio;
+        uint8_t txinv_polarity;
+        uint8_t get_index(void) const {
+            return uint8_t(this - &_serial_tab[0]);
+        }
+    };
+
+    bool wait_timeout(uint16_t n, uint32_t timeout_ms) override;
+
+    void set_flow_control(enum flow_control flow_control) override;
+    enum flow_control get_flow_control(void) override { return _flow_control; }
+
+    // allow for low latency writes
+    bool set_unbuffered_writes(bool on) override;
+
+    void configure_parity(uint8_t v) override;
+    void set_stop_bits(int n) override;
+
+    /*
+      return timestamp estimate in microseconds for when the start of
+      a nbytes packet arrived on the uart. This should be treated as a
+      time constraint, not an exact time. It is guaranteed that the
+      packet did not start being received after this time, but it
+      could have been in a system buffer before the returned time.
+
+      This takes account of the baudrate of the link. For transports
+      that have no baudrate (such as USB) the time estimate may be
+      less accurate.
+
+      A return value of zero means the HAL does not support this API
+     */
+    uint64_t receive_time_constraint_us(uint16_t nbytes) override;
+
+    uint32_t bw_in_kilobytes_per_second() const override {
+        if (sdef.is_usb) {
+            return 200;
+        }
+        return _baudrate/(9*1024);
+    }
+    // request information on uart I/O
+    static void uart_info(ExpandingString &str);
+
+
+	void vprintf(const char *fmt, va_list ap) override;
+
 private:
-    bool _initialized;
-    const size_t TX_BUF_SIZE = 1024;
-    const size_t RX_BUF_SIZE = 1024;
-    uint8_t _buffer[32];
+    const SerialDef &sdef;
+
+    /*
+      copy of rx_line and tx_line with alternative configs resolved
+     */
+    ioline_t atx_line;
+    ioline_t arx_line;
+
+    // thread used for all UARTs
+    static void* volatile uart_rx_thread_ctx;
+
+    // table to find UARTDrivers from serial number, used for event handling
+    static UARTDriver *uart_drivers[UART_MAX_DRIVERS];
+
+    // thread used for writing and reading
+    void* volatile uart_thread_ctx;
+    char uart_thread_name[6];
+
+    // index into uart_drivers table
+    uint8_t serial_num;
+
+    // key for a locked port
+    uint32_t lock_write_key;
+    uint32_t lock_read_key;
+
+    uint32_t _baudrate;
+    const void* _uart_owner_thd;
+
+    struct {
+        // thread waiting for data
+        thread_t *thread_ctx;
+        // number of bytes needed
+        uint16_t n;
+    } _wait;
+
+    // we use in-task ring buffers to reduce the system call cost
+    // of ::read() and ::write() in the main loop
     ByteBuffer _readbuf{0};
     ByteBuffer _writebuf{0};
-    Semaphore _write_mutex;
-    void read_data();
-    void write_data();
+    HAL_Semaphore _write_mutex;
+    volatile bool _in_rx_timer;
+    volatile bool _in_tx_timer;
+    bool _blocking_writes;
+    volatile bool _rx_initialised;
+    volatile bool _tx_initialised;
+    volatile bool _device_initialised;
+    static const SerialDef _serial_tab[];
 
-    uint8_t uart_num;
+    // timestamp for receiving data on the UART, avoiding a lock
+    uint64_t _receive_timestamp[2];
+    uint8_t _receive_timestamp_idx;
+
+    // handling of flow control
+    enum flow_control _flow_control = FLOW_CONTROL_DISABLE;
+    bool _rts_is_active;
+    uint32_t _last_write_completed_us;
+    uint32_t _first_write_started_us;
+    uint32_t _total_written;
+
+    // statistics
+    uint32_t _tx_stats_bytes;
+    uint32_t _rx_stats_bytes;
+    static uint32_t _last_stats_ms;
+
+    // we remember config options from set_options to apply on sdStart()
+    uint32_t _cr1_options;
+    uint32_t _cr2_options;
+    uint32_t _cr3_options;
+    uint16_t _last_options;
+
+    // set to true for unbuffered writes (low latency writes)
+    bool unbuffered_writes;
+
+    static void rxbuff_full_irq(void* self, uint32_t flags);
+    static void tx_complete(void* self, uint32_t flags);
+
+    void update_rts_line(void);
+
+    void check_dma_tx_completion(void);
+
+    void write_pending_bytes_NODMA(uint32_t n);
+    void write_pending_bytes(void);
+    void read_bytes_NODMA();
+
+    void receive_timestamp_update(void);
+
+    // set SERIALn_OPTIONS for pullup/pulldown
+    void set_pushpull(uint16_t options);
+
+    static void thread_rx_init();
+    void thread_init();
+    void uart_thread();
+    static void uart_rx_thread(void* arg);
+    static void uart_thread_trampoline(void* p);
 };
 
 }
